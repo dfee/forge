d7136b09159541c1e1d4eb6fb5a1b03a16185a14
Devin Fee <devin@devinfee.com>
2018-05-24 00:21:02 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -4,15 +4,21 @@ from setuptools import setup
 
 
 def permit_setup():
-    # pylint: disable=W0612, unused-variable
+    """
+    Determines whether setup is permitted:
+    - CPython >= 3.6+
+    - PyPy >= 3.5+
+    :return: True if setup is allowed
+    """
     implementation = platform.python_implementation()
-    ver = sys.version_info
+    v = sys.version_info  # pylint: disable=C0103, invalid-name
 
-    if not any([
-            implementation == 'CPython' and ver.major == 3 and ver.minor >= 6,
-            implementation == 'PyPy' and ver.major == 3 and ver.minor >= 5,
-        ]):
-        raise RuntimeError('CPython 3.6+ or PyPy 3.5+ required')
+    return any([
+        all([implementation == 'CPython', v.major == 3, v.minor >= 6]),
+        all([implementation == 'PyPy', v.major == 3, v.minor >= 5]),
+    ])
 
 if permit_setup():
     setup()
+else:
+    raise RuntimeError('CPython 3.6+ or PyPy 3.5+ required')
